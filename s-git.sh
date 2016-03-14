f9cc7691d799e7097f0cf49526c058aa7eae2ca7
pedroalpacheco <pedro.pacheco.a@gmail.com>
2016-03-14 15:55:36 -0300

    Primeiro_commit

---
+++ s-git.sh
@@ -0,0 +1,6 @@
+ git status 
+ git add -A . 
+ git status 
+ echo "Comentario:"; read com
+ git commit -m $com
+ git push
---
+++ start.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Marco 14 14:00:23 2016
+LCI
+@author: papacheco
+"""
+
+import md5
+import hashlib
+import os
+
+
+arquivo = open('/home/papacheco/Desktop/API-s-python/verifica_hash/teste1.txt', 'r').read()
+
+arquivo2 = open('/home/papacheco/Desktop/API-s-python/verifica_hash/teste2.txt', 'r').read()
+
+lk = hashlib.md5(arquivo)
+lk2 = hashlib.md5(arquivo2)
+
+print(lk)
+print(lk2)
+
+if lk == lk:
+    print("Arquivos iguais!")
+else:
+    print("Arquivos diferentes!")
+
